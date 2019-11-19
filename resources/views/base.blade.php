400cbce67dd304d3a63277089f8d7234a1529fdb
diego <diego.barros@ifms.edu.br>
2019-11-19 10:50:23 -0400

    laravel-crud

---
+++ resources/views/base.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel CRUD Tutorial</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+</head>
+<body>
+  <div>
+  testesss
+  </div>
+  <div class="container">
+    @yield('main')
+  </div>
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+</body>
+</html>
